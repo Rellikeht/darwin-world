acf300376ec735d3fecccd7f28e4ed3755d760d7
Marcin Serafin <Marcin.serafin0325@gmail.com>
2024-01-13 21:41:51 +0100

    Zabawa z prezenterem (jeszcze to naprawei)

--- src/main/java/world/SimulationPresenter.java
+++ src/main/java/world/SimulationPresenter.java
@@ -6,18 +6,26 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import javafx.util.converter.NumberStringConverter;
 import world.model.MapChangeListener;
 import world.model.WorldMap;
 
+import java.util.function.UnaryOperator;
+
 public class SimulationPresenter extends Application implements MapChangeListener {
+
+    public VBox textBox;
+    public ToolBar mapBar;
+    public ToolBar mutationBar;
     @FXML
-    private TextField textField;
+    private Button Basic;
+    @FXML
+    private Button Custom;
     @FXML
     private Button startButton;
     @FXML
@@ -26,27 +34,83 @@ public class SimulationPresenter extends Application implements MapChangeListene
     private GridPane mapGrid;
     @FXML
     private WorldMap map;
-
+    @FXML
+    private ToggleButton HellMap;
+    @FXML
+    private ToggleButton EarthMap;
+    @FXML
+    private ToggleButton SpecialMutation;
+    @FXML
+    private ToggleButton NormalMutation;
+    @FXML
+    private TextField mapWidth;
+    @FXML
+    private TextField mapHeight;
+    @FXML
+    private TextField staringGrass;
+    @FXML
+    private TextField dailyGrass;
+    @FXML
+    private TextField jungleSize;
+    @FXML
+    private TextField animalNumber;
+    @FXML
+    private TextField animalEnergy;
+    @FXML
+    private TextField grassEnergy;
+    @FXML
+    private TextField readyEnergy;
+    @FXML
+    private TextField procreationEnergy;
+    @FXML
+    private TextField minMutation;
+    @FXML
+    private TextField maxMutation;
+    @FXML
+    SimulationSettings settings = new SimulationSettings();
+    boolean basic = false;
     public void drawMap(String message) {
         Platform.runLater(() -> {
             GridMapDrawer drawer = new GridMapDrawer(mapGrid, map);
             drawer.draw();
             moveInfoLabel.setText(message);
         });
-    }
+    };
+    private void uploadSettings(){
+        int widthNum=Integer.parseInt(mapWidth.getText());
+        if(widthNum>0){settings.setMapWidth(widthNum);}
+        int heightNum=Integer.parseInt(mapHeight.getText());
+        if(heightNum>0){settings.setMapHeight(heightNum);}
+        settings.setInitialGrassAmount(Integer.parseInt(staringGrass.getText()));
+        settings.setDailyGrassAmount(Integer.parseInt(dailyGrass.getText()));
+        settings.setJungleSize(Integer.parseInt(jungleSize.getText()));
+        settings.setInitialAnimalAmount(Integer.parseInt(animalNumber.getText()));
+        settings.setInitialAnimalEnergy(Integer.parseInt(animalEnergy.getText()));
+        settings.setGrassEnergy(Integer.parseInt(grassEnergy.getText()));
+        settings.setEnergyNeededForProcreation(Integer.parseInt(readyEnergy.getText()));
+        settings.setEnergyTakenByProcreation(Integer.parseInt(procreationEnergy.getText()));
+        settings.setMinAmountOfMutations(Integer.parseInt(minMutation.getText()));
+        settings.setMaxAmountOfMutations(Integer.parseInt(maxMutation.getText()));
 
+    }
     @Override
     public void mapChanged(WorldMap worldMap, String message) {
         drawMap(message);
     }
-
+    public void setHellMap(){settings.setMapBasic(false);}
+    public void setEarthMap(){settings.setMapBasic(true);}
+    public void setNormalMutation(){settings.setMutationRandom(false);}
+    public void setSpecialMutation(){settings.setMutationRandom(true);}
     public void onSimulationStartClicked(ActionEvent actionEvent) {
-        SimulationSettings settings = new SimulationSettings();
-        // TODO ustawianie ustawień
-
+        if(!basic) {
+            uploadSettings();
+        }
         Simulation simulation = new Simulation(settings);
         map = simulation.getMap();// TODO nie wiem, czy ta mapa tutaj to dobry pomysł
         simulation.addListener(this);
+        mutationBar.setVisible(false);
+        mapBar.setVisible(false);
+        textBox.setVisible(false);
         startButton.setDisable(true);
         simulation.run();
     }
@@ -58,7 +122,20 @@ public class SimulationPresenter extends Application implements MapChangeListene
         primaryStage.minWidthProperty().bind(viewRoot.minWidthProperty());
         primaryStage.minHeightProperty().bind(viewRoot.minHeightProperty());
     }
-
+    public void custom(){
+        mutationBar.setVisible(true);
+        mapBar.setVisible(true);
+        startButton.setDisable(false);
+        textBox.setVisible(true);
+        basic=false;
+    }
+    public void basic(){
+        mutationBar.setVisible(false);
+        mapBar.setVisible(false);
+        startButton.setDisable(false);
+        textBox.setVisible(false);
+        basic=true;
+    }
     @Override
     public void start(Stage primaryStage) throws Exception {
         FXMLLoader loader = new FXMLLoader();
@@ -68,5 +145,6 @@ public class SimulationPresenter extends Application implements MapChangeListene
 
         //SimulationPresenter presenter = loader.getController();
         primaryStage.show();
+
     }
 }
